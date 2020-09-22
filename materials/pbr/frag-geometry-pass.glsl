1c6f4b371fee145d3159f695890bbbe2152c03a4
Sun, Taiyi <suntaiyi2013@163.com>
2020-09-22 10:47:19 +0800

    added deferred pbr material
    removed phong materials

--- materials/pbr/frag-geometry-pass.glsl
+++ materials/pbr/frag-geometry-pass.glsl
@@ -1,15 +1,7 @@
 #version 330
 #extension GL_ARB_shading_language_include : require
 
-#include "phong-light-common.glsl"
-
-struct MaterialUniform
-{
-    sampler2D TextureDiffuse;
-    sampler2D TextureSpecular;
-    sampler2D TextureNormal;
-    float SpecularShiness;
-};
+#include "pbr-common.glsl"
 
 uniform MaterialUniform Material;
 
@@ -22,8 +14,8 @@ layout (location = 0) out vec4 gWorldPosition;
 layout (location = 1) out vec4 gNormal;
 layout (location = 2) out vec4 gTangent;
 layout (location = 3) out vec4 gNormalMap;
-layout (location = 4) out vec4 gDiffuseMap;
-layout (location = 5) out vec4 gSpecularMap;
+layout (location = 4) out vec4 gAlbedoMap;
+layout (location = 5) out vec4 gMRAMap;
 
 void main()
 {
@@ -31,6 +23,11 @@ void main()
     gNormal = vec4(normalize(vNormal), 1.0);
     gTangent = vec4(normalize(vTangent), 1.0);
     gNormalMap = texture(Material.TextureNormal, vCoord);
-    gDiffuseMap = texture(Material.TextureDiffuse, vCoord);
-    gSpecularMap = vec4(texture(Material.TextureSpecular, vCoord).rgb, Material.SpecularShiness);
+    gAlbedoMap = texture(Material.TextureAlbedo, vCoord);
+    gMRAMap = vec4(
+        texture(Material.TextureMetallic, vCoord).r,
+        texture(Material.TextureRoughness, vCoord).r,
+        texture(Material.TextureAmbientOcclusion, vCoord).r,
+        1.0
+    );
 }
--- materials/pbr/frag-lighting-pass.glsl
+++ materials/pbr/frag-lighting-pass.glsl
@@ -1,7 +1,7 @@
 #version 330 core
 #extension GL_ARB_shading_language_include : require
 
-#include "phong-light-common.glsl"
+#include "pbr-common.glsl"
 
 in vec2 vCoord;
 
@@ -13,20 +13,20 @@ struct GBufferUniform
     sampler2D TextureNormal;
     sampler2D TextureTangent;
     sampler2D TextureNormalMap;
-    sampler2D TextureDiffuseMap;
-    sampler2D TextureSpecularMap;
+    sampler2D TextureAlbedoMap;
+    sampler2D TextureMRAMap;
 };
 
 uniform GBufferUniform GBuffer;
 
 uniform LightUniform Light;
 
+uniform float Exposure;
+
 layout (location = 0) out vec4 gColor;
 
 void main()
 {
-    vec4 specularAndShiness = texture(GBuffer.TextureSpecularMap, vCoord);
-
     vec3 T = normalize(texture(GBuffer.TextureTangent, vCoord).xyz);
     vec3 N = normalize(texture(GBuffer.TextureNormal, vCoord).xyz);
     vec3 B = normalize(cross(N, T));
@@ -38,31 +38,35 @@ void main()
     geomParam.viewer = normalize(TBN * ViewerPosition - geomParam.position);
 
     MaterialParam matParam;
-    matParam.diffuse = texture(GBuffer.TextureDiffuseMap, vCoord).rgb;
-    matParam.specular = specularAndShiness.rgb;
-    matParam.shiness = specularAndShiness.a;
+    matParam.albedo = pow(texture(GBuffer.TextureAlbedoMap, vCoord).rgb, vec3(2.2));
+    vec3 mra = texture(GBuffer.TextureMRAMap, vCoord).rgb;
+    matParam.metallic = mra.r;
+    matParam.roughness = mra.g;
+    matParam.ao = mra.b;
+    matParam.f0 = mix(vec3(0.04), matParam.albedo, matParam.metallic);
 
-    vec3 color = vec3(0.0);
+    vec3 Lo = vec3(0.0);
     for (int i = 0; i < Light.LightCount; ++i)
     {
         LightParam lightParam;
         lightParam.position = TBN * Light.Lights[i].Position;
         lightParam.direction = TBN * Light.Lights[i].Direction;
-        lightParam.diffuse = Light.Lights[i].DiffuseColor;
-        lightParam.specular = Light.Lights[i].SpecularColor;
+        lightParam.color = Light.Lights[i].Color;
         lightParam.attenue = Light.Attenue;
         switch (Light.Lights[i].Type)
         {
         case LIGHT_TYPE_D:
-            color += calcPhongLightD(lightParam, geomParam, matParam);
+            Lo += calcPbrLightD(lightParam, geomParam, matParam);
             break;
         case LIGHT_TYPE_P:
-            color += calcPhongLightP(lightParam, geomParam, matParam);
+            Lo += calcPbrLightP(lightParam, geomParam, matParam);
             break;
         }
     }
-    vec3 ambient = Light.AmbientColor * matParam.diffuse;
-    color += ambient;
-    color = clamp(color, 0.0, 1.0);
-    gColor = vec4(clamp(color, 0.0, 1.0), 1.0);
+    vec3 ambient = vec3(0.03) * matParam.albedo * matParam.ao;
+    vec3 color = ambient + Lo;
+	
+    color = ToneMapping(color, Exposure);
+   
+    gColor = vec4(color, 1.0);
 }
